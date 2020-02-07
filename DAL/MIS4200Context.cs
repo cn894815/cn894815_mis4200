365ed18a59074bef312d9e4831fe5eacf5f4f841
Cameron Nagy <cn894815@ohio.edu>
2020-02-07 12:35:31 -0500

    Changes Last Error

--- DAL/MIS4200Context.cs
+++ DAL/MIS4200Context.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
 using System.Linq;
 using System.Web;
 
@@ -12,12 +13,18 @@ namespace cn894815_mis4200.DAL
         public MIS4200Context() : base("name=DefaultConnection")
         {
             // this method is a 'constructor' and is called when a new context is created
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<MIS4200Context, cn894815_mis4200.Migrations.MISContext.Configuration>("DetaultConnection"));
 
         }
         public DbSet<Vet> Vets { get; set; }
         public DbSet<Pet> Pets { get; set; }
         public DbSet<PetDetail> PetDetails { get; set; }
         public DbSet<Owner> Owners { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+        }
 
 
 
---
+++ Migrations/MIS4200Context/Configuration.cs
@@ -0,0 +1,26 @@
+namespace cn894815_mis4200.Migrations.MISContext
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<cn894815_mis4200.DAL.MIS4200Context>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationDataLossAllowed = true;
+            AutomaticMigrationsEnabled = true;
+            MigrationsDirectory = @"Migrations\MIS4200Context";
+            ContextKey = "cn894815_mis4200.DAL.MIS4200Context";
+        }
+
+        protected override void Seed(cn894815_mis4200.DAL.MIS4200Context context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
